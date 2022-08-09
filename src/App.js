efca617fcbfc5fa4b56086d4c1dbd6ae03325030
Andrew <joel_barish@mail.ru>
2022-08-09 14:16:09 +0300

    NiceOne_v3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import Footer from "./Components/Footer/Footer";
-import AppRouter from "./Components/UI/AppRouter";
+import AppRouter from "./Routes/AppRouter";
 import Navbar from "./Components/Navbar/Navbar";
 import "./Styles/App.css";
 
